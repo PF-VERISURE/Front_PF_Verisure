23c13e14a57eb659723e13e888102972289e204e
ManonChab <godfroymanon@gmail.com>
2026-04-20 10:55:37 +0200

    fix : Removed footer from login page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -16,86 +16,60 @@ import AdminOngProfiles from '../components/pages/admin/AdminONGProfiles';
 import Login from '../components/pages/Login/Login';
 import AdminPage from "../components/pages/admin/AdminPage/AdminPage";
 import OngProjects from '../components/pages/ong/OngProject/OngProjects';
+import AuthLayout from '../layout/AuthLayout';
 
 
 export const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/login",
+        Component: Login
+      }
+    ]
+  },
+
   {
     path: "/",
     Component: Layout,
     children: [
       {
         index: true,
-        element: <Rutas />,
+        Component: Rutas,
       },
 
-      {
-        path: "login",
-        Component: Login
-      },
       {
         path: "admin",
         Component: AdminDashboard,
         children: [
-          {
-            index: true,
-            Component: AdminPage
-          },
-          {
-            path: "metricas",
-            Component: AdminMetrics
-          },
-          {
-            path: "proyectos",
-            Component: AdminProject
-          },
-          {
-            path: "ongs/perfiles",
-            Component: AdminOngProfiles
-          },
-          {
-            path: "voluntario/perfiles",
-            Component: AdminVolonteerProfile
-          }
-
+          { index: true, Component: AdminPage },
+          { path: "metricas", Component: AdminMetrics },
+          { path: "proyectos", Component: AdminProject },
+          { path: "ongs/perfiles", Component: AdminOngProfiles },
+          { path: "voluntario/perfiles", Component: AdminVolonteerProfile }
         ]
-
       },
+
       {
         path:"voluntario",
         Component: VolonteerDashboard,
         children:[
-          {
-            index: true,
-            Component: VolonteerExplore
-          },
-          {
-            path: "proyectos",
-            Component: VolonteerProject
-          },
-          {
-            path: "certificados",
-            Component: VolonteerCertificates
-          }
+          { index: true, Component: VolonteerExplore },
+          { path: "proyectos", Component: VolonteerProject },
+          { path: "certificados", Component: VolonteerCertificates }
         ]
       },
+
       {
         path: "ongs",
         Component: OngDashboard,
         children:[
-          {
-            index: true,
-            Component: OngNewProject
-          },
-          {
-            path: "proyectos",
-            Component: OngProjects
-          },
-          {
-            path: "nuevo_proyecto",
-            Component: OngNewProject
-          }
+          { index: true, Component: OngNewProject },
+          { path: "proyectos", Component: OngProjects },
+          { path: "nuevo_proyecto", Component: OngNewProject }
         ]
       },
     ]
   }
-])
+]);
